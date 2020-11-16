e9aa9e100321f4f60e506837b85d3c47a6f72bb0
Choiyounho <maxosa78@gmail.com>
2020-11-16 20:23:54 +0900

    [feat] : 직사각형 별찍기

---
+++ src/main/java/programers/level1/ShootingStars.java
@@ -0,0 +1,19 @@
+package programers.level1;
+
+import java.util.Scanner;
+
+public class ShootingStars {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+
+        for (int i = 0; i < b; i++) {
+            for (int j = 0; j < a; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+}

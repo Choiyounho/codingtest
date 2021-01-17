0d942a6de296bca4c47e6317e9860f6d8ca619d5
Choiyounho <maxosa78@gmail.com>
2021-01-17 23:49:25 +0900

    initial kotlin-codingtest project

--- src/test/java/programers/level1/TrialExamTest.java
+++ src/test/java/programers/level1/TrialExamTest.java
@@ -1,4 +1,18 @@
-import static org.junit.jupiter.api.Assertions.*;
+package programers.level1;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 class TrialExamTest {
-  
+
+    private TrialExam trialExam = new TrialExam();
+    private int[] answer = {1, 2, 3, 4, 5};
+
+    @Test
+    @DisplayName("Java-Level1 : 모의고사")
+    void solution() {
+        assertThat(trialExam.solution(answer)).isEqualTo(new int[]{1});
+    }
 }

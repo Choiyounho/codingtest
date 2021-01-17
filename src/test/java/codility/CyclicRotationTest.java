ac7dddb5c19947a422206645a3a2f4b958cc273b
Choiyounho <maxosa78@gmail.com>
2021-01-17 23:23:12 +0900

    [feat] : CyclicRotation TestCode

---
+++ src/test/java/codility/CyclicRotationTest.java
@@ -0,0 +1,25 @@
+package codility;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+class CyclicRotationTest {
+
+    private CyclicRotation cyclicRotation = new CyclicRotation();
+    private int[] numbers = {3, 8, 9, 7, 6};
+    private int rotation = 3;
+
+    @Test
+    @DisplayName("Java-Codility : CyclicRotation - 100%")
+    void solution() {
+        assertThat(cyclicRotation.solution(numbers, rotation)).isEqualTo(new int[]{9, 7, 6, 3, 8});
+    }
+
+    @Test
+    @DisplayName("Java-Codility : CyclicRotation - 87%, TIME OUT")
+    void solution1() {
+        assertThat(cyclicRotation.solution1(numbers, rotation)).isEqualTo(new int[]{9, 7, 6, 3, 8});
+    }
+}

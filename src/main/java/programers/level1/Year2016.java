3e12210f429e5472e1997e996454c2859a7f34d5
Choiyounho <maxosa78@gmail.com>
2021-03-18 23:12:46 +0900

    [refactor] : 2016년 리팩토링

--- src/main/java/programers/level1/Year2016.java
+++ src/main/java/programers/level1/Year2016.java
@@ -3,49 +3,19 @@ package programers.level1;
 public class Year2016 {
 
     static int[] lastDays = {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-    static String[] days = {"SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"};
+    static String[] days = {"THU", "FRI", "SAT", "SUN", "MON", "TUE", "WED"};
 
     public String solution(int a, int b) {
-        int monthCount = sumLastDay(a, b);
-
-        return resultDay(monthCount);
+        return days[sumLastDay(a, b) % 7];
     }
 
     private int sumLastDay(int a, int b) {
         int sum = 0;
-
         for (int i = 1; i < a; i++) {
             sum += lastDays[i];
         }
 
         return sum + b;
-    }
-
-    private String resultDay(int monthCount) {
-        if (monthCount % 7 == 1) {
-            return days[5];
-        }
-
-        if (monthCount % 7 == 2) {
-            return days[6];
-        }
-
-        if (monthCount % 7 == 3) {
-            return days[0];
-        }
-
-        if (monthCount % 7 == 4) {
-            return days[1];
-        }
-
-        if (monthCount % 7 == 5) {
-            return days[2];
-        }
-
-        if (monthCount % 7 == 6) {
-            return days[3];
-        }
-        return days[4];
     }
 
 }

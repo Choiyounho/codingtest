49f5e251e735825a5bd8b98b4bc6b0f23c4839cf
Choiyounho <maxosa78@gmail.com>
2021-04-30 23:30:44 +0900

    [feat] : NumberOfProvinces

---
+++ src/main/java/leetcode/NumberOfProvinces.java
@@ -0,0 +1,72 @@
+package leetcode;
+
+public class NumberOfProvinces {
+
+    public int findCircleNum(int[][] isConnected) {
+        // 자기 자신을 0으로 만든다.
+        toZeroForMe(isConnected);
+
+        int answer = checkSolo(isConnected);
+
+        // 이어진 링크들 다 0으로 만들기
+        answer = getAnswer(isConnected, answer);
+
+        return answer;
+    }
+
+    private void toZeroForMe(int[][] isConnected) {
+        for (int i = 0; i < isConnected.length; i++) {
+            isConnected[i][i] = 0;
+        }
+    }
+
+    private int checkSolo(int[][] isConnected) {
+        int answer = 0;
+
+        boolean isZero = true;
+        // 모든 값이 0이면 answer++
+        for (int i = 0; i < isConnected.length; i++) {
+            for (int j = 0; j < isConnected[0].length; j++) {
+                if (isConnected[i][j] != 0) { // 0이 아닐때 isZero 변경
+                    isZero = false;
+                    break;
+                }
+            }
+            if (isZero) {
+                answer++;
+                continue;
+            }
+            isZero = true;
+        }
+        return answer;
+    }
+
+    private int getAnswer(int[][] isConnected, int answer) {
+        boolean isCheck = false;
+        for (int i = 0; i < isConnected.length; i++) {
+            for (int j = 0; j < isConnected[0].length; j++) {
+                if (isConnected[i][j] != 0) {
+                    isCheck = true;
+                    dfs(isConnected, i, j);
+                }
+            }
+            if (isCheck) {
+                answer++;
+                isCheck = false;
+            }
+        }
+        return answer;
+    }
+
+    private void dfs(int[][] isConnected, int i, int j) {
+        isConnected[i][j] = 0;
+        isConnected[j][i] = 0;
+
+        for (int k = 0; k < isConnected[0].length; k++) {
+            if (isConnected[j][k] != 0) {
+                dfs(isConnected, j, k);
+            }
+        }
+    }
+
+}
---
+++ src/test/java/leetcode/NumberOfProvincesTest.java
@@ -0,0 +1,32 @@
+package leetcode;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+class NumberOfProvincesTest {
+
+    private final NumberOfProvinces numberOfProvinces = new NumberOfProvinces();
+
+    @Test
+    @DisplayName("Java-Medium : Number-of-provinces")
+    void test() {
+        assertThat(numberOfProvinces.findCircleNum(new int[][]{
+                {1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0},
+                {0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0},
+                {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0},
+                {0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0},
+                {0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0},
+                {1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0},
+                {0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0},
+                {0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0},
+                {0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1}})).isEqualTo(8);
+    }
+}

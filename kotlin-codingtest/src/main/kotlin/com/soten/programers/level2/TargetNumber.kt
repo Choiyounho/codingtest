3ab64370c67932768736e300504e22fcde1b98e2
Choiyounho <maxosa78@gmail.com>
2021-06-03 03:07:47 +0900

    [feat] : (kotlin) 타겟 넘버

---
+++ kotlin-codingtest/src/main/kotlin/com/soten/programers/level2/TargetNumber.kt
@@ -0,0 +1,30 @@
+package com.soten.programers.level2
+
+import java.util.*
+
+var answer = 0
+
+fun solution(numbers: IntArray, target: Int): Int {
+	targeting(target, 0, numbers, 0)
+	return answer
+}
+
+fun targeting(target: Int, index: Int, numbers: IntArray, cycle: Int) {
+	if (cycle == target && index == numbers.size) {
+		answer++
+	}
+
+	if (index >= numbers.size) {
+		return
+	}
+
+	val plusCycle = cycle + numbers[index]
+	val minusCycle = cycle - numbers[index]
+
+	targeting(target, index + 1, numbers, plusCycle)
+	targeting(target, index + 1, numbers, minusCycle)
+}
+
+fun main() {
+	println(solution(intArrayOf(1, 1, 1, 1, 1), 3))
+}

da7ce804b120933ac7870b0b59bcc92bf5090f10
Choiyounho <maxosa78@gmail.com>
2021-06-03 03:05:47 +0900

    [feat] : (kotlin) 콜라츠 추측

---
+++ kotlin-codingtest/src/main/kotlin/com/soten/programers/level1/Cloatz.kt
@@ -0,0 +1,21 @@
+package com.soten.programers.level1
+
+
+fun solution(num: Int): Int {
+	var answer = 0
+	var number = num
+	while (true) {
+		if (number == 1) {
+			return answer
+		}
+		number = if (number % 2 == 0) number / 2 else number * 3 + 1
+		answer++
+		if (answer == 500) {
+			return -1
+		}
+	}
+}
+
+fun main() {
+	println(solution(6))
+}

a6aaf38359cf1c7a30cc4b3f5e8197ecea2272ae
Choiyounho <maxosa78@gmail.com>
2021-06-22 14:25:34 +0900

    [refactor] : (java) 모의고사

--- src/main/java/programers/level1/TrialExam.java
+++ src/main/java/programers/level1/TrialExam.java
@@ -1,50 +1,34 @@
 package programers.level1;
 
+import java.util.*;
+
 // 모의 고사
 public class TrialExam {
 
     public int[] solution(int[] answers) {
-        int[] mathLoser1 = {1, 2, 3, 4, 5};
-        int[] mathLoser2 = {2, 1, 2, 3, 2, 4, 2, 5};
-        int[] mathLoser3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+        int[] case1 = {1, 2, 3, 4, 5}; // 5개
+        int[] case2 = {2, 1, 2, 3, 2, 4, 2, 5}; // 8개
+        int[] case3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5}; // 10개
 
-        int[] loserPoint = new int[3];
-
-        int loser1Circle = getLoserCircle(answers.length, mathLoser1.length);
-        loserPoint[0] = getLoserPoint(answers, mathLoser1, loserPoint[0], loser1Circle, 5);
-
-        int loser2Circle = getLoserCircle(answers.length, mathLoser2.length);
-        loserPoint[1] = getLoserPoint(answers, mathLoser2, loserPoint[1], loser2Circle, 8);
-
-        int loser3Circle = getLoserCircle(answers.length, mathLoser3.length);
-        loserPoint[2] = getLoserPoint(answers, mathLoser3, loserPoint[2], loser3Circle, 10);
-
-        if (loserPoint[0] == loserPoint[1] && loserPoint[1] == loserPoint[2]) return new int[]{1, 2, 3};
-
-        if (loserPoint[0] == loserPoint[1] && loserPoint[0] > loserPoint[2]) return new int[]{1, 2};
-
-        if (loserPoint[1] == loserPoint[2] && loserPoint[1] > loserPoint[0]) return new int[]{2, 3};
-
-        if (loserPoint[0] == loserPoint[2] && loserPoint[0] > loserPoint[1]) return new int[]{1, 3};
-
-        if (loserPoint[0] > loserPoint[1] && loserPoint[0] > loserPoint[2]) return new int[]{1};
-
-        if (loserPoint[1] > loserPoint[0] && loserPoint[1] > loserPoint[2]) return new int[]{2};
-
-        return new int[]{3};
-    }
-
-    private int getLoserCircle(int length, int length2) {
-        return length % length2;
-    }
-
-    private int getLoserPoint(int[] answers, int[] mathLoser, int loserPoint, int loserCircle, int pattern) {
+        int[] score = {0, 0, 0};
         for (int i = 0; i < answers.length; i++) {
-            if (answers[i] == mathLoser[getLoserCircle(answers.length + i - loserCircle, pattern)]) {
-                loserPoint++;
-            }
+            if (case1[i % case1.length] == answers[i]) score[0]++;
+            if (case2[i % case2.length] == answers[i]) score[1]++;
+            if (case3[i % case3.length] == answers[i]) score[2]++;
         }
-        return loserPoint;
-    }
 
+        int maxScore = Math.max(score[0], Math.max(score[1], score[2]));
+
+        List<Integer> list = new ArrayList<>();
+        if (maxScore == score[0]) list.add(1);
+        if (maxScore == score[1]) list.add(2);
+        if (maxScore == score[2]) list.add(3);
+
+        int[] answer = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            answer[i] = list.get(i);
+        }
+
+        return answer;
+    }
 }

f0863fa7f4adf22114b075d511c98074009465f6
Choiyounho <maxosa78@gmail.com>
2021-10-31 22:07:01 +0900

    [feat] : (kotlin) leetcode - BestTimetoBuyandSellStock

--- kotlin-codingtest/src/main/kotlin/com/soten/leetcode/easy/BestTimetoBuyandSellStock.kt
+++ kotlin-codingtest/src/main/kotlin/com/soten/leetcode/easy/BestTimetoBuyandSellStock.kt
@@ -1,6 +1,6 @@
 package com.soten.leetcode.easy
 
-fun maxProfit(prices: IntArray): Int {
+private fun maxProfit(prices: IntArray): Int {
 	if (prices.size == 1) {
 		return 0
 	}
@@ -9,24 +9,9 @@ fun maxProfit(prices: IntArray): Int {
 	var maxBenefit = 0
 
 	for (price in prices) {
-		minBenefit = minBenefit.coerceAtMost(price) // Math.min(minBenefit, i)
+		minBenefit = minBenefit.coerceAtMost(price)
 		maxBenefit = maxBenefit.coerceAtLeast(price - minBenefit)
 	}
-	//   0  900 1 39 999 99999
-	// 100 1000 1 40 1000 100000
 
 	return maxBenefit
 }
-
-fun main() {
-	println(maxProfit(intArrayOf(100, 1000, 1, 40, 1000, 100000)))
-}
-
-
-/*
-*
-* dp를 사용
-*
-*
-* 최소값과 현재 위치 가격과 비교
-* */
